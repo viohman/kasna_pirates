f81078fd0f2b9b15300b0f30910b1ce7620afa50
viohman <julek@post.pl>
2021-08-13 05:05:12 +0000

    first commit

--- main.py
+++ main.py
@@ -22,13 +22,41 @@ logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-moves = ['F', 'T', 'L', 'R']
+moves = ['F', 'L', 'R']
 
 @app.route("/", methods=['POST'])
 def move():
     request.get_data()
     logger.info(request.json)
+    logger.info("Test test test")
+
+    content = request.get_json()
+    me = content["_links"]["self"]["href"]
+
+    arena_dims = content["arena"]["dims"]
+    arena_state = content["arena"]["state"]
+    my_location = [arena_state[me]["x"], arena_state[me]["y"]]
+    my_dir = arena_state[me]["direction"]
+
+    arena_state.pop(me)
+
+    for pirate in arena_state:
+
+        if pirate['x'] == my_location[0] and pirate['x'] < my_location[0] and my_dir == "W":
+            return "T"   
+        if pirate['x'] == my_location[0] and pirate['x'] > my_location[0] and my_dir == "E":
+            return "T"
+        if pirate['y'] == my_location[1] and pirate['y'] < my_location[0] and my_dir == "S":
+            return "T"
+        if pirate['y'] == my_location[1] and pirate['y'] > my_location[0] and my_dir == "N":
+            return "T"
+
+         
+
     return moves[random.randrange(len(moves))]
+
+
+
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
